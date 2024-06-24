95cfb54251adf97375cf19d32fe2da8122471c3c
singsingmanki <122100659+singsingmanki@users.noreply.github.com>
2024-06-24 22:54:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ const element = document.getElementById("myBtn");
 
 element.addEventListener("click", myFunction);
 
-
 async function myFunction() {
-
-
 
     document.getElementById("xyz").innerHTML = '沒有這條路線';
 
@@ -19,22 +16,14 @@ async function myFunction() {
 
     let name1 = document.getElementById("lname").value.toUpperCase();
 
-
-
-
     for(let x in data.data){
-
 
         if(data.data[x].route === name1){
 
         if(data.data[x].bound === "O"){
 
             document.getElementById("xyz").innerHTML = '請選擇路線：';
-
-
-
-
-
+        }
 
             let xxlam = data.data[x].orig_tc
             let yylam = data.data[x].dest_tc
@@ -47,46 +36,17 @@ async function myFunction() {
           document.getElementById("xyz").appendChild(xx)
 
           xx.addEventListener("click", xFunction)
-
-
-
-
-
-
         } 
 
-        if(data.data[x].bound === "I"){
-
-
-
-
-                let xlam = data.data[x].orig_tc
-                let ylam = data.data[x].dest_tc
-
-                  let yy = document.createElement("button");
-                  yy.classList.add("m");
-
-                  yy.innerText =   xlam + "至" + ylam
-
-
-              document.getElementById("xyz").appendChild(yy)
-
-              yy.addEventListener("click", yFunction)
-
-
-
-
-
-            }}
-
-
-
-
-
-
-
+     
 
           async function xFunction(){
+
+            if(data.data[x].bound === "I"){
+                bound = "inbound"}
+
+                if(data.data[x].bound === "O"){
+                    bound = "outbound"}
 
             document.getElementById("xxx").innerHTML = ""
 
@@ -96,9 +56,6 @@ async function myFunction() {
 
             let service_type = data.data[x].service_type
 
-            let bound = "outbound"
-
-
 
             const api2 = `https://data.etabus.gov.hk/v1/transport/kmb/route-stop/${route}/${bound}/${service_type}`
             const res2 = await fetch(api2);
@@ -107,28 +64,21 @@ async function myFunction() {
 
            for(let dicky in data2.data){
 
-
-
             let gg = document.createElement("button");
             gg.classList.add("m");
 
         let stop = data2.data[dicky].stop
         
-
-
-
             const api3 = `https://data.etabus.gov.hk/v1/transport/kmb/stop/${stop}`
 
             const res3 = await fetch(api3);
 
             const data3 = await res3.json();
 
-
-
             let door = document.createElement("button");
             door.classList.add("d");
 
-            door.innerText = [parseInt(dicky)+1] + "." +  data3.data.name_tc
+            door.innerText = [parseInt(dicky)+1] + ") " +  data3.data.name_tc
 
             document.getElementById("xxx").appendChild(door)
 
@@ -142,9 +92,7 @@ async function myFunction() {
 
                 const data4 = await res4.json();
 
-
-
-                let xbutton = document.createElement("button");
+                let xbutton = document.createElement("div");
 
                 xbutton.classList.add("xbutton")
 
@@ -152,35 +100,33 @@ async function myFunction() {
 
 
 
-                console.log(data2.data[dicky].seq)
-               
 
-
+    
                 for(j in data4.data){
-
-                    
-              
 
                     console.log(data4.data[j].seq)
 
-
-            
-
                     if(data4.data[j].seq == data2.data[dicky].seq){
-
-
-
-
 
                     let doo = document.createElement("div");
 
-                    doo.classList.add("d");
+                    doo.classList.add("e");
+
+                    if(data4.data[j].rmk_tc == ""){
+                        data4.data[j].rmk_tc = "實時班次"
+
+                    }
+
+                    let cd = j
+                    if(cd>=3){
+                        cd=cd-3}
 
 
+                    let ggg = data4.data[j].eta
+                    gggg= ggg.toString().slice(11,18)
 
-
-                    doo.innerText = [parseInt(j)+1] + "." + data3.data.name_tc +"\n" + data4.data[j].eta +
-                    `(${data4.data[j].rmk_tc})`
+                    doo.innerText = [parseInt(cd)+1] + ") " + data3.data.name_tc +"\n" + gggg +
+                    +"\n"+"\n"  + `(${data4.data[j].rmk_tc})`  +"\n" +"\n" 
 
             document.getElementById("yyy").appendChild(doo).appendChild(xbutton)
 
@@ -190,108 +136,7 @@ async function myFunction() {
             function removefunction(){
                 document.getElementById("yyy").removeChild(doo)
             }}}}
-
-
-
-
-
-
         }
-
-
-
     }
-
-    async function yFunction(){
-
-                document.getElementById("xxx").innerHTML = ""
-
-                document.getElementById("yyy").innerHTML = ""
-
-        let route = data.data[x].route
-
-        let service_type = data.data[x].service_type
-
-        let bound = "inbound"
-
-
-
-
-        const api2 = `https://data.etabus.gov.hk/v1/transport/kmb/route-stop/${route}/${bound}/${service_type}`
-        const res2 = await fetch(api2);
-
-       const data2 = await res2.json();
-
-       for(let dicky in data2.data){
-
-
-
-        let gg = document.createElement("button");
-        gg.classList.add("m");
-
-    let stop = data2.data[dicky].stop
-
-
-
-
-            const api3 = `https://data.etabus.gov.hk/v1/transport/kmb/stop/${stop}`
-
-            const res3 = await fetch(api3);
-
-            const data3 = await res3.json();
-
-
-
-            let door = document.createElement("button");
-            door.classList.add("d");
-
-            door.innerText = [parseInt(dicky)+1] + "." +  data3.data.name_tc
-
-            document.getElementById("xxx").appendChild(door)
-
-            door.addEventListener("click", zzFunction)
-
-            async function zzFunction(){
-
-                const api4 = `https://data.etabus.gov.hk/v1/transport/kmb/eta/${stop}/${route}/${service_type}`
-
-                const res4 = await fetch(api4);
-
-                const data4 = await res4.json();
-
-
-
-                let xbutton = document.createElement("button");
-
-                xbutton.classList.add("xbutton")
-
-                xbutton.innerText = "X"
-
-
-                for(j in data4.data){
-
-                    if(data4.data[j].seq == data2.data[dicky].seq){
-
-
-
-
-
-                        let doo = document.createElement("div");
-    
-                        doo.classList.add("d");
-    
-    
-    
-    
-                        doo.innerText = [parseInt(j)+1] + "." + data3.data.name_tc +"\n" + data4.data[j].eta +
-                        `(${data4.data[j].rmk_tc})`
-    
-                document.getElementById("yyy").appendChild(doo).appendChild(xbutton)
-    
-    
-                xbutton.addEventListener("click",removefunction)
-    
-                function removefunction(){
-                    document.getElementById("yyy").removeChild(doo)
-            }}
-        }}}}}}
+   
+    }}
